ba89050e6e96de4d86964140a211d96353584da2
Aaditi2205 <malhotraaaditi22@gmail.com>
2025-06-10 21:46:55 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =144553
+let accountEmail="aaditi@google.com"
+var accountPassworrd="12345"
+accountCity="Jaipur"
+
+
+//accountId=2
+
+accountEmail = "hc@hc.com"
+accountPassworrd= "21212121"
+accountCity="Bengaluru"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassworrd, accountCity])
