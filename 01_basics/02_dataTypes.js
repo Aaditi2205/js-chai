608166e7c6ca1af30614f7ff56907f28ce18ba19
Aaditi2205 <malhotraaaditi22@gmail.com>
2025-06-10 21:39:36 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+ let name="Aaditi"
+ let age =19
+ let isLoggedIn=false
+let state;
+
+ //number => 2 to power 53
+ // bigint
+ //string
+ //boolean
+ //null=>stand alone value
+ //undefined=> 
+//symbol =? unique
+
+
+ //object
+
+ console.log(typeof "Aaditi");
+ 
+
+//null is a type of  object
